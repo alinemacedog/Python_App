9839cd8134328e347f5cc73365d06db2f2767aa5
alinemacedog <aline_macedog@hotmail.com>
2024-05-28 09:31:27 -0300

    Projeto Completo

---
+++ exercicios.py
@@ -0,0 +1,10 @@
+print('Python na Escola de Programação da Alura')
+
+nome = 'Aline'
+idade = 33
+print(f'''Meu nome é {nome} e tenho {idade} anos''')
+
+print('A','L','U','R','A',sep='\n')
+
+pi = 3.14159
+print(f'O valor arredondado de pi é: {pi:.2f}')
